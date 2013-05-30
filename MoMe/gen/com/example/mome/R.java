a26d7c2c6cfa649f8c25dbd196357fe9ce904105
May <maycamp@umail.ucsb.edu>
2013-05-29 20:57:53 -0700

    added universal strings for us to use

--- MoMe/gen/com/example/mome/R.java
+++ MoMe/gen/com/example/mome/R.java
@@ -8,6 +8,9 @@
 package com.example.mome;
 
 public final class R {
+    public static final class array {
+        public static final int mome_dialog_quote=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,18 +27,60 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int ideas=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int addedittask_duedatequestion=0x7f050023;
+        public static final int addedittask_textfield_nameoftask=0x7f050022;
+        public static final int addedittask_timequestion=0x7f050024;
         public static final int app_name=0x7f050000;
+        public static final int button_todolist=0x7f05000a;
+        public static final int category_habits=0x7f05002b;
+        public static final int category_home=0x7f050028;
+        public static final int category_job=0x7f050025;
+        public static final int category_recreation=0x7f050027;
+        public static final int category_school=0x7f050026;
+        public static final int category_shopping=0x7f05002a;
+        public static final int category_social=0x7f050029;
         public static final int hello_world=0x7f050002;
+        public static final int mome_button_motivateme=0x7f05000d;
+        public static final int mome_dialog_duedate=0x7f05000c;
+        public static final int mome_dialog_welcome=0x7f05000b;
+        public static final int motivateme_button_todolist=0x7f05000f;
+        public static final int motivateme_dialog_welcome=0x7f05000e;
+        public static final int task_always=0x7f05001f;
+        public static final int task_button_back=0x7f050015;
+        public static final int task_button_completed=0x7f050013;
+        public static final int task_button_edit=0x7f050014;
+        public static final int task_categorycolon=0x7f050019;
+        public static final int task_colorcolon=0x7f05001a;
+        public static final int task_duecolon=0x7f050017;
+        public static final int task_extremely=0x7f05001e;
+        public static final int task_importancecolon=0x7f05001b;
+        public static final int task_namecolon=0x7f050016;
+        public static final int task_never=0x7f050020;
+        public static final int task_notatall=0x7f05001d;
+        public static final int task_notecolon=0x7f050021;
+        public static final int task_reminderscolon=0x7f05001c;
+        public static final int task_timerequiredcolon=0x7f050018;
+        public static final int title_addedittask=0x7f050007;
+        public static final int title_help=0x7f050009;
+        public static final int title_mome=0x7f050003;
+        public static final int title_motivateme=0x7f050004;
+        public static final int title_settings=0x7f050008;
+        public static final int title_task=0x7f050006;
+        public static final int title_todolist=0x7f050005;
+        public static final int todolist_button_addtask=0x7f050011;
+        public static final int todolist_button_expand=0x7f050012;
+        public static final int todolist_dialog_welcome=0x7f050010;
     }
     public static final class style {
         /** 
@@ -59,10 +104,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
