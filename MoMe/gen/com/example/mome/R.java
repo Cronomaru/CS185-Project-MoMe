b7e3709ff3b3656c295e14317954e411d47a3a14
May <maycamp@umail.ucsb.edu>
2013-06-05 16:10:41 -0700

    adding name

--- MoMe/gen/com/example/mome/R.java
+++ MoMe/gen/com/example/mome/R.java
@@ -24,25 +24,27 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int avatar_owl_01=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo_idea=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000a;
-        public static final int button1=0x7f090000;
-        public static final int button2=0x7f090001;
-        public static final int button3=0x7f090002;
-        public static final int textView1=0x7f090009;
-        public static final int textView2=0x7f090006;
-        public static final int textView3=0x7f090007;
-        public static final int textView4=0x7f090008;
-        public static final int textView_due=0x7f090004;
-        public static final int textView_name=0x7f090003;
-        public static final int textView_timerequired=0x7f090005;
+        public static final int action_settings=0x7f09000b;
+        public static final int avatar=0x7f090000;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int button3=0x7f090003;
+        public static final int textView1=0x7f09000a;
+        public static final int textView2=0x7f090007;
+        public static final int textView3=0x7f090008;
+        public static final int textView4=0x7f090009;
+        public static final int textView_due=0x7f090005;
+        public static final int textView_name=0x7f090004;
+        public static final int textView_timerequired=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_task=0x7f030001;
-        public static final int ideas=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -66,6 +68,7 @@ public final class R {
     	 				extremely
     	 				always
     	 				never
+
     
          */
         public static final int app_name=0x7f050000;
@@ -115,11 +118,13 @@ public final class R {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
