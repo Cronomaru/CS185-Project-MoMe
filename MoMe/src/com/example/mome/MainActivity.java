4693a755095bcf3a961c75d329f354b9a6872a6b
Lupita Davila <contact@lupitadavila.com>
2013-06-07 01:12:44 -0700

    fixing bug? main

--- MoMe/src/com/example/mome/MainActivity.java
+++ MoMe/src/com/example/mome/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	        
 		case R.id.avatar:
 			// change image
-			if (touches < picList.size() ){				
+			if (touches < (picList.size()-1) ){				
 				((ImageView) v).setImageResource(picList.get(touches));
 				touches++;
 			}else{
