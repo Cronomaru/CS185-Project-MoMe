eb0efeae7742e8b6391125f42f3ac32b08c40747
May <maycamp@umail.ucsb.edu>
2013-06-06 19:11:11 -0700

    blah

--- MoMe/gen/com/example/mome/R.java
+++ MoMe/gen/com/example/mome/R.java
@@ -28,8 +28,11 @@ public final class R {
         public static final int avatar_social=0x7f020001;
         public static final int avatar_todo=0x7f020002;
         public static final int avatar_todo_suggestion=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int logo=0x7f020005;
+        public static final int avatar_todo_suggestion_task=0x7f020004;
+        public static final int avatar_welcome=0x7f020005;
+        public static final int avatar_whatmotivation=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int logo=0x7f020008;
     }
     public static final class id {
         public static final int ScrollView1=0x7f090000;
